79966d4f0f95ee07cf1066c629e873b29222a018
Regalus27 <16656788+Regalus27@users.noreply.github.com>
2024-01-19 12:06:22 -0700

    Added cart removal

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -17,6 +17,40 @@ export function getParam(params) {
   return product;
 }
 
+// @param
+// key: identifier for cart to remove product from
+// id: product id to remove
+// removeAllMatchingItems: if true, remove all products with id
+//    otherwise, remove only the first item encountered
+export function removeItemFromLocalStorage(key, id, removeAllMatchingItems = false) {
+  const cart = getLocalStorage(key);
+  let newCart = [];
+  let haveRemoved = false;
+
+  // I feel like a var i = 0 loop may be more efficient, as I could
+  // end early and splice the rest directly to the new cart.
+  for (const element of cart) {
+    if (element.Id != id) {
+      // always add to new cart if not a match
+      newCart.push(element);
+    }
+    else {
+      // first time removing, skip rest of loop
+      if (!haveRemoved) {
+        haveRemoved = true;
+        continue;
+      }
+      // if NOT removing all items
+      // AND have already removed an item
+      // add to new cart
+      if (!removeAllMatchingItems && haveRemoved) {
+        newCart.push(element);
+      }
+    }
+  }
+  setLocalStorage(key, newCart);
+}
+
 // render a list of products in HTML using a given template function
 export function renderListWithTemplate(
   templateFunction,         // Function used to render the product
