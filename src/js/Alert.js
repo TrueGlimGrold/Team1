b343c12416f0adeb3d5f03598099be330fec6e93
luke bullock <111459215+TrueGlimGrold@users.noreply.github.com>
2024-01-19 18:54:47 -0700

    alert fix done

---
+++ src/js/Alert.js
@@ -0,0 +1,40 @@
+import { renderListWithTemplate } from "./utils.mjs";
+
+export default class Alert {
+    constructor(element) {
+        this.path = `../json/alert.json`;
+        this.element = element;
+    }
+    getData() {
+        return fetch(this.path)
+        .then(convertToJson)
+        .then((data) => data);
+    }
+    async init() {
+        const alertList = await this.getData();
+        const template = generateAlertTemplate(alertList);
+        console.log(template);
+        this.element.insertAdjacentHTML("afterbegin", template);
+    }
+};
+
+function generateAlertTemplate (alertList) {
+    let template = "";
+    template += `<section class="alert-list">`
+    alertList.forEach(alert => {
+    template += `\n <p background=${alert.background} color=${alert.color}>${alert.message}</p>`
+    });
+    template += `\n</section>`
+    return template;
+    }
+
+// const element = document.querySelector("divider");
+
+function convertToJson(res) {
+    if (res.ok) {
+        return res.json();
+    } else {
+        throw new Error("Bad Response");
+    }
+}
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,16 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import Alert from "./Alert.js";
 
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
 const productListing = new ProductList("Tents", dataSource, element);
 productListing.init();
+
+// Make an alert (Luke did this)
+
+const alertElement = document.getElementById("alert-holder");
+
+const alert = new Alert(alertElement); 
+alert.init();
+// console.log(alert);
