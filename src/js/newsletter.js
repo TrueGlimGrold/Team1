515e6b52d5a671f8625d0f316b5f11aaea3bc6d7
kjirstenhernandez <122486287+kjirstenhernandez@users.noreply.github.com>
2024-02-02 21:30:55 -0700

    Testing newsletter.js

--- src/js/newsletter.js
+++ src/js/newsletter.js
@@ -1,6 +1,22 @@
-const closeButton = document.querySelector("#nl-close");
+import { getParam, loadHeaderFooter, setClick } from "./utils.mjs";
+
+// close button variables
 const popUp = document.querySelector(".newsletter-container");
 
-closeButton.addEventListener("click", () => {
+// submit variables
+const submit = document.querySelector("#nl-submit");
+
+setClick("#nl-close", () => {
     popUp.style.display = "none";
-});
+})
+
+submit.addEventListener("click", (event) => {
+    event.preventDefault();
+    const firstName = getParam("firstName")
+    const lastName = getParam("lastName")
+    const email = getParam("email")
+    console.log(firstName, lastName, email)
+    location.replace("../newsletter/success.html")
+  });
+
+
