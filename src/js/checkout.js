6c2d1408475bacac6b0e5cebde5edfc28ac519e1
EmilianoTB <emilianotorres28@hotmail.com>
2024-02-08 17:39:27 -0600

    W04 Individual Activity

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -3,10 +3,30 @@ import CheckoutProcess from "./CheckoutProcess.mjs";
 import { getShoppingCartKey } from "./utils.mjs";
 
 
+
 const check = new CheckoutProcess(getShoppingCartKey(), ".order-summary");
 check.init();
 
 console.log(getLocalStorage(getShoppingCartKey()));
+
+
+document.querySelector(".checkoutSubmit")
+.addEventListener("click", (e) => {
+  e.preventDefault();
+  const myForm = document.forms[0];
+  const check_form = myForm.checkValidity();
+  myForm.reportValidity()
+  if (check_form) {
+    // If form is valid, redirect to another HTML page
+    window.location.href = "success.html"; 
+    localStorage.clear();
+  } else {
+    // If form is not valid, continue with your existing logic
+    check.checkout();
+  }
+}); 
+
+
 
 
 loadHeaderFooter(
