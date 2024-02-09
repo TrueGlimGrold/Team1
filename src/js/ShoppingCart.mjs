779f6c51e1cf9821f1a887d3039713957d378c11
EmilianoTB <emilianotorres28@hotmail.com>
2024-02-09 00:29:41 -0600

    Cart-quantities

--- src/js/ShoppingCart.mjs
+++ src/js/ShoppingCart.mjs
@@ -11,6 +11,7 @@ export default class ShoppingCart {
 
     init() {
         this.renderCart();
+        this.change_value();
     }
 
     bindRemoveButtons() {
@@ -23,6 +24,9 @@ export default class ShoppingCart {
             }
           });
     }
+
+
+
 
     getCart() {
         return getLocalStorage(this.cartKey);
@@ -61,7 +65,48 @@ export default class ShoppingCart {
         }
         this.bindRemoveButtons();
     }
+
+    /**
+    * @param product - json of the product to be rendered.
+    */
+
+    change_value(){
+        let cart = this.getCart();
+        let total = this.getTotal();
+        const change_number = document.getElementById("change_number");
+            change_number.addEventListener("input", function(){
+                //alert("num has changed");
+
+                
+                // Get the value of the input
+                const inputValue = change_number.value;
+
+                //console.log("Input value:", inputValue);
+
+                
+                cart.forEach(element => {
+                    const final_total = Number(element.FinalPrice) * Number(inputValue);
+                    //console.log(final_total);
+                    document.getElementById("get_final_price").innerHTML = `Total: $${final_total.toFixed(2)}`;
+                    //localStorage.setItem("FinalPrice", final_total);
+
+    
+                    const cartItem = localStorage.getItem("so-cart");
+                    const cart = JSON.parse(cartItem);
+                    cart.forEach(item => {
+                        if (item.hasOwnProperty("FinalPrice")) {
+                            item.FinalPrice = final_total;
+                        }
+                    });
+
+                    // Convert the modified cart back to a string and store it back in localStorage
+                    localStorage.setItem('so-cart', JSON.stringify(cart));
+                });
+                
+            });
+        }
 }
+
 
 /**
  * @param product - json of the product to be rendered.
@@ -78,8 +123,9 @@ function cartItemTemplate(product) {
       <h2 class="card__name">${product.Name}</h2>
     </a>
     <p class="cart-card__color">${product.Colors[0].ColorName}</p>
-    <p class="cart-card__quantity">qty: ${product.Quantity}</p>
-    <p class="cart-card__price">$${(Number(product.FinalPrice) * Number(product.Quantity)).toFixed(2)}</p>
+    <input type="number" id="change_number" class="cart-card__quantity" value="${product.Quantity}" min="1" >
+    
+    <p id="final_price" class="cart-card__price">$${(Number(product.FinalPrice) * Number(product.Quantity)).toFixed(2)}</p>
     <button class="cart-remove__button" data-id="${product.Id}">Remove</button>
   </li>`;
 }
@@ -90,7 +136,7 @@ function cartItemTemplate(product) {
 function cartTotalTemplate(totalPrice) {
     return `<li class="cart-card cart-price divider" id="cart-total">
 
-        <p class="cart-card__total_price">Total: $${totalPrice.toFixed(2)}<p>
+        <p id="get_final_price" class="cart-card__total_price">Total: $${totalPrice.toFixed(2)}<p>
         <a href="/checkout/index.html">
             <button>checkout</button>
         </a>
@@ -102,3 +148,4 @@ function emptyCartTemplate() {
     <p>Your cart is empty.</p>
     </li>`;
 }
+
